4b5d61f0fc7bec5ca5f5f6f3ad072055259c1358
Alex Harris <chasenyc@gmail.com>
2017-05-29 00:25:21 -0400

    Initial repository.

---
+++ gowiki.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+    "html/template"
+	"io/ioutil"
+    "net/http"
+    "regexp"
+    "gopkg.in/mgo.v2"
+    "gopkg.in/mgo.v2/bson"
+    "os"
+    "fmt"
+)
+
+type Page struct {
+    Title string
+    Body  []byte
+    Tags  []string
+}
+
+func (p Page) ConvertedBody() template.HTML {
+    search := regexp.MustCompile("\\[([a-zA-Z]+)\\]")
+
+    body := search.ReplaceAllFunc(p.Body, func(s []byte) []byte {
+        m := string(s[1 : len(s)-1])
+        return []byte("<a href=\"/view/" + m + "\">" + m + "</a>")
+    })
+
+    return template.HTML(string(body[:]))
+}
+
+var templates = template.Must(template.ParseFiles("tmpl/edit.html", "tmpl/view.html"))
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+func (p *Page) save() (info *mgo.ChangeInfo, err error) {
+    session, _ := mgo.Dial(getMongo())
+    defer session.Close()
+
+    session.SetMode(mgo.Monotonic, true)
+
+    c := session.DB("testwiki").C("pages")
+
+    return c.Upsert(bson.M{"title": p.Title}, p)
+}
+
+func writeFile(filename string, body []byte) (err error) {
+    return ioutil.WriteFile("data/" + filename, body, 0600)
+}
+
+func readFile(filename string) (text []byte, err error) {
+    return ioutil.ReadFile("data/" + filename)
+}
+
+func loadPage(title string) (*Page, error) {
+    session, err := mgo.Dial(getMongo())
+    defer session.Close()
+
+    // Optional. Switch the session to a monotonic behavior.
+    session.SetMode(mgo.Monotonic, true)
+    c := session.DB("testwiki").C("pages")
+    result := Page{}
+    err = c.Find(bson.M{"title": title}).One(&result)
+    if err != nil {
+        return nil, err
+    }
+    return &Page{Title: result.Title, Body: result.Body}, nil
+
+    // filename := title + ".txt"
+    // body, err := readFile(filename)
+    // if err != nil {
+    //     return nil, err
+    // }
+    // return &Page{Title: title, Body: body}, nil
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+    p, err := loadPage(title)
+    if err != nil {
+        http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+        return
+    }
+    renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
+    p, err := loadPage(title)
+    if err != nil {
+        p = &Page{Title: title}
+    }
+    renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+    body := r.FormValue("body")
+    p := &Page{Title: title, Body: []byte(body)}
+    _, err := p.save()
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+    err := templates.ExecuteTemplate(w, tmpl+".html", p)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+}
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        m := validPath.FindStringSubmatch(r.URL.Path)
+        if m == nil {
+            http.NotFound(w, r)
+            return
+        }
+        fn(w, r, m[2])
+    }
+}
+
+func redirectFront(w http.ResponseWriter, r *http.Request) {
+    http.Redirect(w, r, "/view/FrontPage", http.StatusFound)
+}
+
+func main() {
+    http.HandleFunc("/view/", makeHandler(viewHandler))
+    http.HandleFunc("/edit/", makeHandler(editHandler))
+    http.HandleFunc("/save/", makeHandler(saveHandler))
+    http.HandleFunc("/", redirectFront)
+    http.ListenAndServe(getPort(), nil)
+}
+
+// Get the Port from the environment so we can run on Heroku
+func getPort() string {
+	port := os.Getenv("PORT")
+	// Set a default port if there is nothing in the environment
+	if port == "" {
+		port = "4747"
+		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
+	}
+	return ":" + port
+}
+
+func getMongo() string {
+    uri := os.Getenv("MONGOLAB_URL")
+
+    if uri == "" {
+        fmt.Printf("Can't connect to mongo, no uri provided.")
+        os.Exit(1)
+    }
+
+    return uri
+}
